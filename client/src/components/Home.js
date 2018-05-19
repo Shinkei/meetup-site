12f7dd24dda56fafb66875e6e7d53d420f95272f
shinkei <ing.jorgeivanramirezh@gmail.com>
2018-05-19 18:36:21 -0500

    Creacion del componente que tiene la ubicacion y el mini mapa, por ahira no funciona, parece un error con el key de google

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import AppBar from 'material-ui/AppBar'
+import GeoInformation from './GeoInformation'
 
 class Home extends Component {
   render () {
     return (
       <article>
         <AppBar showMenuIconButton={false} />
+        <h1>{this.props.title}</h1>
+        <p>{this.props.description}</p>
+        <GeoInformation />
       </article>
     )
   }
--- client/src/tests/components/Home.test.js
+++ client/src/tests/components/Home.test.js
@@ -5,8 +5,8 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 describe('Home Component', () => {
   it('renders without crashing', () => {
-	  const div = document.createElement('div')
-	  ReactDOM.render(<MuiThemeProvider><Home /></MuiThemeProvider>, div)
-	  ReactDOM.unmountComponentAtNode(div)
+    const div = document.createElement('div')
+    ReactDOM.render(<MuiThemeProvider><Home /></MuiThemeProvider>, div)
+    ReactDOM.unmountComponentAtNode(div)
   })
 })
